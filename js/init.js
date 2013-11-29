eb56e9da624b30361c911c2aa5b448f49824e0a1
Eldar <eldar@iteh.org>
2013-11-29 11:51:44 +0200

    head_slider

--- js/init.js
+++ js/init.js
@@ -4,5 +4,38 @@ $(document).ready(function(){
     captions: true,
     controls: false,
     pagerCustom: '.b-pager'
-  });  
+  }); 
+  $('.b-head_wslider-1').show();
+  var slider = $('.b-head_slider-1, .b-head_slider-2, .b-head_slider-3').bxSlider({
+    mode: 'fade',
+    auto: true,
+    captions: true,
+    controls: true
+  });
+  
+  $('.bxpager_item-1').click(function() {
+    $('.bxpager_item').removeClass('active');
+    $(this).addClass('active');
+    $('.b-head_wslider-2').hide();
+    $('.b-head_wslider-3').hide();
+    $('.b-head_wslider-1').show();
+    slider.reloadSlider();
+  });
+  $('.bxpager_item-2').click(function() {
+    $('.bxpager_item').removeClass('active');
+    $(this).addClass('active');
+    $('.b-head_wslider-1').hide();
+    $('.b-head_wslider-3').hide();
+    $('.b-head_wslider-2').show();
+    slider.reloadSlider();
+  });
+  $('.bxpager_item-3').click(function() {
+    $('.bxpager_item').removeClass('active');
+    $(this).addClass('active');
+    $('.b-head_wslider-1').hide();
+    $('.b-head_wslider-2').hide();
+    $('.b-head_wslider-3').show();
+    slider.reloadSlider();
+  });
+  
 });
